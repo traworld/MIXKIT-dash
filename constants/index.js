9a24401f40d2530f73cd03c1c506d82f4e7793aa
Sahil Singh <sahilmac145@gmail.com>
2024-02-17 01:39:57 +0530

    Initial commit

---
+++ constants/index.js
@@ -0,0 +1,74 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/i1.jpg',
+    title: 'AI music',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/i3.jpg',
+    title: 'Music store',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/i4.jpg',
+    title: 'Collaborate',
+  },
+ 
+];
+
+export const startingFeatures = [
+  'Stuck finding free sampes : Try our AI music generator',
+  'Want a place to collaborate : Try our collaborate portal',
+  'Want premium packs : Try our Ecommerce for Music Packs with minimal price',
+];
+
+export const newFeatures = [
+
+  {
+    imgUrl: '/headset.svg',
+    title: 'One stop solution',
+    subtitle:
+        'Unite, Create, Collaborate - Your Ultimate Music Hub Online.',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/rec.svg',
+    title: 'AI music genertator',
+    subtitle:
+        'Lorem',
+  },
+  {
+    imgUrl: '/collaborate.svg',
+    title: 'Premium packs',
+    subtitle:
+        'Lorem',
+  },
+  {
+    imgUrl: '/set1.svg',
+    title: 'Collaborate',
+    subtitle:
+        'Lorem',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
